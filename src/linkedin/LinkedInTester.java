eaf42e71e90c97008e6ea75f1ab4a1a0c1df103b
hrishi <hrishi.jiet@gmail.com>
2025-03-18 20:55:35 +0530

    Exercise01
    linkedlist
    append,
    prePrend
    removeFirst
    removeLast
    get
    set
    insert

---
+++ src/linkedin/LinkedInTester.java
@@ -0,0 +1,87 @@
+package linkedin;
+
+public class LinkedInTester {
+
+    public static void main(String[] args) {
+
+        // Test append and delete last
+        LinkedList linkedList = new LinkedList(4);
+        linkedList.append(5);
+        linkedList.append(6);
+        linkedList.getHead();
+        linkedList.getTail();
+        linkedList.removeLastItem();
+        //linkedList.removeLastItem();
+        //linkedList.removeLastItem();
+        linkedList.printList();
+
+        linkedList.getLength();
+
+        // test pre -pend
+
+        System.out.println(" Test Prepend ");
+        LinkedList prependList = new LinkedList(1);
+        prependList.append(2);
+        prependList.append(3);
+        prependList.printList();
+        prependList.prePendItem(4);
+        prependList.printList();
+        prependList.getLength();
+
+        System.out.println(" Using default constructor");
+        LinkedList prependList1 = new LinkedList();
+        prependList1.printList();
+        prependList1.prePendItem(4);
+        prependList1.printList();
+        prependList1.getLength();
+        prependList1.getHead();
+        prependList1.getTail();
+
+
+        System.out.println(" Check remove first");
+        LinkedList removeFirst = new LinkedList();
+        removeFirst.printList();
+        removeFirst.removeFirst();
+        System.out.println("With element");
+        LinkedList removeFirst1 = new LinkedList(1);
+        //removeFirst1.append(2);
+        //removeFirst1.append(3);
+        removeFirst1.printList();
+        removeFirst1.removeFirst();
+        removeFirst1.getLength();
+        //removeFirst1.getHead();
+        //removeFirst1.getTail();
+
+
+        System.out.println(" get element via index");
+        LinkedList getList = new LinkedList();
+        getList.append(1);
+        getList.append(2);
+        getList.append(4);
+        getList.append(4);
+        getList.append(3);
+        System.out.println(getList.get(0).value);
+
+        System.out.println(" set element via index");
+        LinkedList setList = new LinkedList();
+        setList.append(1);
+        setList.append(2);
+        setList.append(4);
+        setList.append(3);
+        System.out.println(setList.set(2,68));
+        setList.printList();
+
+        System.out.println(" insert element via index");
+        LinkedList insertList = new LinkedList();
+        insertList.append(1);
+        insertList.append(2);
+        insertList.append(4);
+        insertList.append(3);
+        insertList.printList();
+        System.out.println(insertList.insert(2,68));
+        System.out.println("after insert");
+        insertList.printList();
+
+
+    }
+}
---
+++ src/linkedin/LinkedList.java
@@ -0,0 +1,207 @@
+package linkedin;
+
+public class LinkedList {
+
+    private Node head;
+    private Node tail;
+    private int length;
+
+    public static class Node {
+        int value;
+        Node next;
+
+        public Node(int value) {
+            this.value = value;
+        }
+
+    }
+
+    public LinkedList() {
+    }
+
+    public LinkedList(int value) {
+        Node newNode = new Node(value);
+        head = newNode;
+        tail = newNode;
+        length = 1;
+    }
+
+    public void printList() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.println(temp.value);
+            temp = temp.next;
+        }
+    }
+
+    public void append(int value) {
+        Node newNode = new Node(value);
+        if (length == 0) {
+            // head updated only when linkedList is empty
+            head = newNode;
+            tail = newNode;
+        } else {
+            tail.next = newNode;
+            tail = newNode;
+        }
+        length++;
+    }
+
+    /**
+     * assign temp and pre with head,
+     * as we first check if linkedList is empty or not
+     * <p>
+     * if not empty, find if next node of Head is null if null, point head and tail to null deleting node
+     * reduce length
+     * <p>
+     * if next element is not null keep iterating until we fine and assign pre to current temp.next
+     * so when we do find last element , pre would be pointing to second last element
+     * now point tail to pre and tail.next to null
+     * reduce length
+     */
+    public void removeLastItem() {
+        Node temp = head;
+        Node pre = head;
+        if (length != 0) {
+            while (temp.next != null) {
+                pre = temp;
+                temp = temp.next;
+
+            }
+            tail = pre;
+            tail.next = null;
+            length--;
+            if (length == 0) {
+                head = null;
+                tail = null;
+            }
+        }
+    }
+
+    /**
+     * two conditions,
+     * one if existing linkedList having value
+     * then point head to new node and next to old head
+     * <p>
+     * if not values create node assign head and tail.
+     *
+     * @param value
+     */
+    public void prePendItem(int value) {
+
+        Node newHead = new Node(value);
+        if (length == 0) {
+            head = newHead;
+            tail = newHead;
+        } else {
+            newHead.next = head;
+            head = newHead;
+        }
+        length++;
+
+    }
+
+    /**
+     * check if list is not empty
+     * then check with it contains only one value
+     * assign head and tail to null if only one element
+     * <p>
+     * if more than one element
+     * then assign head to temp.next. next element of Head
+     * reduce length
+     */
+    public void removeFirst() {
+        if (length != 0) {
+            Node temp = head;
+            if (length == 1) {
+                head = null;
+                tail = null;
+            } else {
+                head = temp.next;
+            }
+            length--;
+        }
+    }
+
+    /**
+     * check if list is empty, index is less than 0 or greater or equal to length return null
+     * for loop till index retrun index
+     * @param index
+     * @return
+     */
+    public Node get(int index) {
+
+        if(length==0||index<0 || index>=length) return null;
+        if(index==0)
+            return head;
+        if(index==length-1)
+            return tail;
+        Node temp =head;
+        for(int i=0; i<index ;i++ ){
+            temp = temp.next;
+        }
+        return temp;
+
+    }
+
+    /**
+     * get node with get() methos
+     * check if null ot not
+     * if not null set new vlaue to current temp node
+     * return true
+     * else return false
+     * @param index
+     * @param value
+     * @return
+     */
+    public boolean set(int index, int value) {
+        Node temp = get(index);
+        if (temp!=null) {
+            temp.value = value;
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * check empty and index validity
+     * if index 0, then use prePendItem
+     * if last index, use append to insert
+     * find element with get(index-1) // as we need element one node before index
+     * create new node and replace new node next with Node received from get(index) call
+     * now change node.next value of get(index-1) with new node.
+     * we have added node successfully
+     * @param index
+     * @param value
+     * @return
+     */
+    public boolean insert(int index, int value){
+        if(length==0|| index<0|| index>length) return false;
+        if(index==0){
+            prePendItem(value);
+            return true;
+        }
+        if(index==length-1){
+            append(value);
+            return true;
+        }
+        Node temp = get(index-1);
+        Node newNode = new Node(value);
+        newNode.next = temp.next;
+        temp.next = newNode;
+        length++;
+        return true;
+    }
+
+    public void getHead() {
+        System.out.println("Head " + head.value);
+    }
+
+    public void getTail() {
+        System.out.println("Tail " + tail.value);
+    }
+
+    public void getLength() {
+        System.out.println("Length " + length);
+    }
+}
